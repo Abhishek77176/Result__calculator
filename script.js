f5da54aae4a012d7deb3d5483a41906bef43f8a1
Abhishek77176 <109688544+Abhishek77176@users.noreply.github.com>
2024-01-10 11:05:50 +0000

    5th Semester

--- script.js
+++ script.js
@@ -13,44 +13,44 @@ function calculateSGPA(event){
   event.preventDefault();
 
   // Getting marks of theory subject
-  var pas_m = parseInt(document.getElementById('pas').value);
-  var pas_gpc = gpc(pas_m,100,3);
+  var java_m = parseInt(document.getElementById('java').value);
+  var java_gpc = gpc(java_m,100,4);
 
-  var dbms_m = parseInt(document.getElementById('dbms').value);
-  var dbms_gpc = gpc(dbms_m,100,3);
+  var dm_m = parseInt(document.getElementById('dm').value);
+  var dm_gpc = gpc(dm_m,100,4);
 
-  var pp_m = parseInt(document.getElementById('pp').value);
-  var pp_gpc = gpc(pp_m,100,3);
+  var iot_m = parseInt(document.getElementById('iot').value);
+  var iot_gpc = gpc(iot_m,100,3);
 
-  var os_m = parseInt(document.getElementById('os').value);
-  var os_gpc = gpc(os_m,100,3);
+  var toc_m = parseInt(document.getElementById('toc').value);
+  var toc_gpc = gpc(toc_m,100,4);
 
-  var wt_m = parseInt(document.getElementById('wt').value);
-  var wt_gpc = gpc(wt_m,100,3);
+  var el_m = parseInt(document.getElementById('el').value);
+  var el_gpc = gpc(el_m,100,3);
 
-  var cam_m = parseInt(document.getElementById('cam').value);
-  var cam_gpc = gpc(cam_m,100,3);
+  // var cam_m = parseInt(document.getElementById('cam').value);
+  // var cam_gpc = gpc(cam_m,100,3);
 
   // Getting marks of Lab subject
-  var dbmsl_m = parseInt(document.getElementById('dbmsl').value);
-  var dbmsl_gpc = gpc(dbmsl_m,50,1);
+  var javal_m = parseInt(document.getElementById('javal').value);
+  var javal_gpc = gpc(javal_m,50,2);
 
-  var ppl_m = parseInt(document.getElementById('ppl').value);
-  var ppl_gpc = gpc(ppl_m,50,1);
+  var iotl_m = parseInt(document.getElementById('iotl').value);
+  var iotl_gpc = gpc(iotl_m,50,1);
 
-  var osml_m = parseInt(document.getElementById('osml').value);
-  var osml_gpc = gpc(osml_m,50,1);
+  var ell_m = parseInt(document.getElementById('ell').value);
+  var ell_gpc = gpc(ell_m,50,1);
 
-  var wtl_m = parseInt(document.getElementById('wtl').value);
-  var wtl_gpc = gpc(wtl_m,50,1);
+  // var wtl_m = parseInt(document.getElementById('wtl').value);
+  // var wtl_gpc = gpc(wtl_m,50,1);
 
-  // Mentoring Marks
-  var mpd_m = parseInt(document.getElementById('mpd').value);
-  var mpd_gpc = gpc(mpd_m,100,1);
+  // Training Marks
+  var trii_m = parseInt(document.getElementById('trii').value);
+  var trii_gpc = gpc(trii_m,100,1);
 
-  var sum = pas_m + dbms_m + pp_m + os_m + wt_m + cam_m + ppl_m + osml_m + wtl_m + mpd_m + dbmsl_m;
-  document.getElementById("demo").textContent = "Your Percentage is : " + (sum*100/900).toFixed(2) + "%";
-  var SGPA = (pas_gpc + dbms_gpc + pp_gpc + os_gpc + wt_gpc + cam_gpc + dbmsl_gpc + ppl_gpc + osml_gpc + wtl_gpc + mpd_gpc)/23;
+  var sum = java_m + dm_m + iot_m + toc_m + el_m + javal_m + iotl_m + ell_m + trii_m;
+  document.getElementById("demo").textContent = "Your Percentage is : " + (sum*100/750).toFixed(2) + "%";
+  var SGPA = (java_gpc + dm_gpc + iot_gpc + toc_gpc + el_gpc + javal_gpc + iotl_gpc + ell_gpc + trii_gpc)/23;
   document.getElementById("demo2").textContent = "Your SGPA is : " + (SGPA).toFixed(2);
 
   const element = document.getElementsByClassName('down')[0];
@@ -58,10 +58,20 @@ function calculateSGPA(event){
 
   var targetElement = document.getElementById("dm");
 
-  // Scroll to the target element
   targetElement.scrollIntoView({ behavior: "smooth" });
 }
 var currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
+
+function calculateCGPA(event){
+  event.preventDefault();
+  var sem_1 = parseFloat(document.getElementById('sem1').value);
+  var sem_2 = parseFloat(document.getElementById('sem2').value);
+  var sem_3 = parseFloat(document.getElementById('sem3').value);
+  var sem_4 = parseFloat(document.getElementById('sem4').value);
+  var sem_5 = parseFloat(document.getElementById('sem5').value);
+  var total = ((sem_1*20) + (sem_2*21) + (sem_3*21) + (sem_4*23) + (sem_5*23))/108;
+  document.getElementById('demo3').textContent = "Your CGPA is : " + total.toFixed(2);
+}
 
 
